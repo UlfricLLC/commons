5c6bdfcc130cbc7a95663bbf3527fa5373114eff
Adam Harrison <ad@mharrison.us>
2017-09-01 13:14:39 -0400

    Handle some TODOs

--- src/main/java/com/ulfric/commons/text/RegexHelper.java
+++ src/main/java/com/ulfric/commons/text/RegexHelper.java
@@ -2,15 +2,19 @@ package com.ulfric.commons.text;
 
 import com.ulfric.commons.collection.MapHelper;
 
-import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 import java.util.regex.Pattern;
 
 public class RegexHelper {
 
-	private static final Map<String, Pattern> PATTERNS = MapHelper.newConcurrentMap(1);
+	private static final ConcurrentMap<String, Pattern> PATTERNS = MapHelper.newConcurrentMap(1);
 
 	public static Pattern compile(String text) {
 		return PATTERNS.computeIfAbsent(text, Pattern::compile);
+	}
+
+	public static boolean matches(String stringInQuestion, String pattern) {
+		return matches(stringInQuestion, compile(pattern));
 	}
 
 	public static boolean matches(String stringInQuestion, Pattern pattern) {
