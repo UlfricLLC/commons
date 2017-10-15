9693ecc80507b4a25b6e0f7e04b8bea57e812c82
Adam Harrison <ad@mharrison.us>
2017-10-15 13:02:30 +0300

    FutureHelper#allOf

--- src/main/java/com/ulfric/commons/concurrent/FutureHelper.java
+++ src/main/java/com/ulfric/commons/concurrent/FutureHelper.java
@@ -1,6 +1,9 @@
 package com.ulfric.commons.concurrent;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class FutureHelper {
 
@@ -9,6 +12,14 @@ public class FutureHelper {
 	@SuppressWarnings("unchecked")
 	public static <T> CompletableFuture<T> emptyCompletableFuture() {
 		return (CompletableFuture<T>) EMPTY;
+	}
+
+	@SafeVarargs
+	public static <T> CompletableFuture<List<T>> allOf(CompletableFuture<T>... futures) {
+		return CompletableFuture.allOf(futures)
+				.thenApply(ignore -> Arrays.stream(futures)
+						.map(CompletableFuture::join)
+						.collect(Collectors.toList()));
 	}
 
 	private FutureHelper() {
