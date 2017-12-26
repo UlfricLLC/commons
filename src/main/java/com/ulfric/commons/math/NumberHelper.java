79f181e8c2e7898bdf393f195f51b20c5c20811d
Adam Harrison <ad@mharrison.us>
2017-12-26 12:21:46 -0500

    updates

--- src/main/java/com/ulfric/commons/math/NumberHelper.java
+++ src/main/java/com/ulfric/commons/math/NumberHelper.java
@@ -1,6 +1,7 @@
 package com.ulfric.commons.math;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.OptionalInt;
 
 import org.apache.commons.lang3.math.NumberUtils;
@@ -20,8 +21,22 @@ public class NumberHelper {
 		}
 	}
 
+	public static Optional<BigDecimal> parseBigDecimal(String string) {
+		try {
+			string = string.replace(",", "");
+			BigDecimal value = NumberUtils.createBigDecimal(string);
+			return Optional.ofNullable(value);
+		} catch (NumberFormatException notNumber) {
+			return Optional.empty();
+		}
+	}
+
 	public static boolean isNegative(BigDecimal number) {
 		return number.compareTo(BigDecimal.ZERO) < 0;
+	}
+
+	public static boolean isPositive(BigDecimal number) {
+		return number.compareTo(BigDecimal.ZERO) > 0;
 	}
 
 	private NumberHelper() {
